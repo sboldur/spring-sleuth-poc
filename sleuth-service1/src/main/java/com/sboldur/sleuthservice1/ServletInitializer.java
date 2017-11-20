64b41e631529af16072810ddda547e654bcb9f8a
Simona Boldur <simonaboldur@Simonas-MacBook-Pro.local>
2017-11-20 20:04:50 +0100

    initial commit

---
+++ sleuth-service1/src/main/java/com/sboldur/sleuthservice1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sboldur.sleuthservice1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SleuthService1Application.class);
+	}
+
+}
---
+++ sleuth-service1/src/main/java/com/sboldur/sleuthservice1/SleuthService1Application.java
@@ -0,0 +1,12 @@
+package com.sboldur.sleuthservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthService1Application.class, args);
+	}
+}
